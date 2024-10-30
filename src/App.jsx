4813ef4e88d370c5fa7d730db590486f51e3b723
isaladwy <isaladwy@gmail.com>
2024-10-30 22:46:39 +0300

    setting the main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Loader from './components/Loader';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     async function fetchProducts() {
-      const res = await fetch('https://fakestoreapi.com/products');
-      const data = await res.json();
-      console.log(data);
+      try {
+        const res = await fetch('https://fakestoreapi.com/products');
+        if (!res.ok) throw new Error('Failed to fetch products');
+        const data = await res.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     }
-    fetchProducts()
+    fetchProducts();
   }, []);
 
-  return <div></div>;
+  if (loading) return <Loader />;
+  if (error)
+    return <div className="text-center text-red-500 py-20">{error}</div>;
+
+  return (
+    <div>
+      <h1>Product Listing</h1>
+      <div>
+        {products.map((product) => (
+          <span>Product ({product.id})</span>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
